b954ab9e5925e4390035d81a97d6acb58d4e9ec1
Adam <adamvhayward@hotmail.com>
2019-09-23 21:07:51 -0400

    fixes

--- assets/main.js
+++ assets/main.js
@@ -1,29 +1,30 @@
 function dogImage() {
-  let number = $(".js-query").val();
-  var originalURL = `https://dog.ceo/api/breed/hound/images/random/${number}`;
-  var queryURL = "https://cors-anywhere.herokuapp.com/" + originalURL;
-  fetch(queryURL)
-    .then(response => response.json())
-    .then(responseJson => {
-      for (i = 0; i < responseJson.message.length; i++) {
-        console.log(responseJson.message[i]);
-		$(".results").addClass("active");
-        $(".results").prepend(
-           `<img  src='${responseJson.message[i]} >`
-        );
-      }
-    })
-    .catch(error => alert("Something went wrong. Try again later."));
-}
-
-function clickForm() {
-  $(".form-list").submit(event => {
-    event.preventDefault();
-    dogImage();
+	let number = $(".js-query").val();
+	var originalURL = `https://dog.ceo/api/breed/hound/images/random/${number}`;
+	var queryURL = "https://cors-anywhere.herokuapp.com/" + originalURL;
+	fetch(queryURL)
+	  .then(response => response.json())
+	  .then(responseJson => {
+		for (i = 0; i < responseJson.message.length; i++) {
+			console.log(responseJson.message[i]);
+			  $('.results').addClass('active');
+			  $('.results').prepend(`<img  src="${responseJson.message[i]}" />`);
+		}
+	  })
+	  .catch(error => alert("Something went wrong. Try again later."));
+  }
+  
+  function clickForm() {
+	$(".form-list").submit(event => {
+	  event.preventDefault();
+	  dogImage();
+	});
+  }
+  
+  
+  
+  $(function() {
+	console.log("app loaded");
+	clickForm();
   });
-}
-
-$(function() {
-  console.log("app loaded");
-  clickForm();
-});
+  
