725cf4a113cc3c6ce6c8421021f81ac4196a59d0
Adam <adamvhayward@hotmail.com>
2019-09-28 03:26:13 -0400

    fixes changes

--- assets/main.js
+++ assets/main.js
@@ -1,7 +1,7 @@
-function dogImage() {
+function dogImageBreed() {
   let number = $(".js-query").val();
   let breed = $(".dog-selector option:selected").val().replace("-", "/");
-  const originalURL = `https://dog.ceo/api/breed/${breed}/images/random/${number}`;
+  const originalURL = `https://dog.ceo/api/breed/hound/images/random/${number}`;
   const queryURL = "https://cors-anywhere.herokuapp.com/" + originalURL;
   fetch(queryURL)
     .then(response => response.json())
@@ -16,17 +16,36 @@ function dogImage() {
     });
 }
 
+function dogImageNumber() {
+  let number = $(".js-query").val();
+  let breed = $(".dog-selector option:selected").val().replace("-", "/");
+  const originalURL = `https://dog.ceo/api/breed/${breed}/images/random/1`;
+  const queryURL = "https://cors-anywhere.herokuapp.com/" + originalURL;
+  fetch(queryURL)
+    .then(response => response.json())
+    .then(responseJson => {
+      for (i = 0; i < responseJson.message.length; i++) {
+        console.log(responseJson.message[i]);
+        $(".results").addClass("active");
+        $(".results").prepend(
+          `<img  class="images" src="${responseJson.message[i]}" />`
+        );
+      }
+    });
+}
+
+
 function clickForm() {
   $(".form-list-one ").submit(event => {
     event.preventDefault();
-    dogImage();
+    dogImageBreed();
   });
-}``
+}
 
 function clickFormTwo(){
   $('.form-list-two').submit(event => {
     event.preventDefault();
-    dogImage();
+    dogImageNumber();
   })
 }
 
